4b84c81f03a038e7c32c1d582f5a814efb8c3992
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-05-05 13:01:46 -0400

    wortks

--- js/script.js
+++ js/script.js
@@ -2,26 +2,17 @@
 // ICS2O-Unit5-04-HTML
 // May 2 2022
 
-'use strict'
-/**
- * Check service worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-04-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-04-HTML/",
-  })
-}
+"use strict";
 
-/**
- * Checks if the user is eligible for student pricing for a museum depending on their age and the current day of the week.
- */
-function onButtonClick() {
+function myButtonClicked() {
   const age = document.getElementById("age").value
-  const day = document.getElementById("days").value
+  const day = document.getElementById("days").value 
 
-  if (day == "tuesday" || day == "thursday" && age > 12 && age < 22) {
-    document.getElementById("result").innerHTML = "<h5>You can pay with the museum's student pricing.</h5>"
-  } else {
-    document.getElementById("result").innerHTML = "<h5>You must pay with the museum's regular pricing.</h5>"
+  
+  if ((day == "tuesday") || (day == "thursday") || (age > 12 && age < 21)) {
+    document.getElementById("answers").innerHTML = "You're eligible for student pricing!"
+  }
+  else {
+    document.getElementById("answers").innerHTML = "You must pay regular price."
   }
 }
