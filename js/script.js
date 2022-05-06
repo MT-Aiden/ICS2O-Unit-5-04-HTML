1977d973602b35bb56efc798ba2764a2b0d28ddb
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-05-06 13:09:13 -0400

    commenyt

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@
 // May 2 2022
 
 "use strict";
-
+/**
+ * Check service worker.
+ */
 function myButtonClicked() {
   const age = document.getElementById("age").value
   const day = document.getElementById("days").value 
 
-  
+  /**
+ * Checks if the user is eligible for student pricing for a museum depending on their age and the current day of the week.
+ */
   if ((day == "tuesday") || (day == "thursday") || (age > 12 && age < 21)) {
     document.getElementById("answers").innerHTML = "You're eligible for student pricing!"
   }
